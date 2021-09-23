371bae2d7f9c734517badf5713f088ce20bac331
zoqol <37377691+zoqol@users.noreply.github.com>
2021-09-23 03:25:43 -0700

    Add files via upload

---
+++ controls.js
@@ -0,0 +1,19 @@
+const { Panel,Knob, Button, Canvas, HBox, Toggle, Defaults, Dropdown, Label,Window } = mc2;
+const holder=document.querySelector('#holder');
+
+const win = new Window(document.body, "Window", 480, 20, 485, 100);
+
+
+for( [i,r] of Globals.R.entries()){
+    console.log(r,i)
+    const f1 = new Knob(win,
+        10+i*480/8, 10, "", r*100, 0, r*100,render);
+        f1.index=i;
+        
+}
+
+  function render(v){
+     
+      
+      Globals.R[v.target.index]=v.detail/100;
+  }

649e8773dc43866685a93bb242e823c933df88fc
zoqol <37377691+zoqol@users.noreply.github.com>
2021-09-23 04:11:06 -0700

    Add files via upload

--- controls.js
+++ controls.js
@@ -6,8 +6,9 @@ const win = new Window(document.body, "Window", 480, 20, 485, 100);
 
 for( [i,r] of Globals.R.entries()){
     console.log(r,i)
+    var perc=r*100;
     const f1 = new Knob(win,
-        10+i*480/8, 10, "", r*100, 0, r*100,render);
+        10+i*480/8, 10, "", r*100, 0, 100,render);
         f1.index=i;
         
 }
